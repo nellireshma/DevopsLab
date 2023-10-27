ceac7cbb05d14aabec53cc3916203df77efce2e9
nellireshma <94431945+nellireshma@users.noreply.github.com>
2023-10-27 14:56:38 +0530

    Add files via upload

---
+++ src/main/java/com/lbrce/devops/UserValidation.java
@@ -0,0 +1,17 @@
+package com.lbrce.devops;
+
+import java.util.ResourceBundle;
+
+public class UserValidation {
+	public boolean check(String uname,String pswd)
+	{
+		ResourceBundle rb=ResourceBundle.getBundle("config");
+		String username=rb.getString("username");
+		String password=rb.getString("password");
+		if(uname.equals(username)&&pswd.equals(password))
+			return true;
+		else
+			return false;
+		
+	}
+}
